1ad9a5419286bae7843e3e40caac6c9be26f14a0
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-04-13 16:35:47 +0800

    16:35

--- anjian.py
+++ anjian.py
@@ -40,8 +40,8 @@ def find_subHandle(pHandle, winClassList):
 
 '''
 #进入单证模块,选择ebooking
-docuwindow = win32gui.FindWindow('WindowsForms10.Window.8.app.0.2a2cc74_r9_ad1','Document ApplicationNBZRP0')
-document = win32gui.FindWindowEx(docuwindow,0,'WindowsForms10.Window.8.app.0.2a2cc74_r9_ad1','menuStrip1')
+docuwindow = win32gui.FindWindow('WindowsForms10.Window.8.app.0.3b93019_r11_ad1','Document ApplicationNBZRP0')
+document = win32gui.FindWindowEx(docuwindow,0,'WindowsForms10.Window.8.app.0.3b93019_r11_ad1','menuStrip1')
 win32gui.SetForegroundWindow(document)
 win32api.keybd_event(win32con.VK_RIGHT,0,0,0)
 win32api.keybd_event(win32con.VK_RIGHT,0,win32con.KEYEVENTF_KEYUP,0)
@@ -56,16 +56,25 @@ win32gui.PostMessage(document, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 time.sleep(.2)
 '''
 #e-booking
-docuwindow = win32gui.FindWindow('WindowsForms10.Window.8.app.0.2a2cc74_r9_ad1','Document ApplicationNBZRP0')
-backwindow = win32gui.FindWindowEx(docuwindow,0,'WindowsForms10.MDICLIENT.app.0.2a2cc74_r9_ad1',None)
-bookingwindow = win32gui.FindWindowEx(backwindow,0,'WindowsForms10.Window.8.app.0.2a2cc74_r9_ad1','Accept E-booking')
-bookingtitle = find_idxSubHandle(bookingwindow,'WindowsForms10.Window.8.app.0.2a2cc74_r9_ad1',4)
-vslvoy = find_idxSubHandle(bookingtitle,'WindowsForms10.Window.8.app.0.2a2cc74_r9_ad1',0)
-vslname = find_idxSubHandle(vslvoy,'WindowsForms10.Window.b.app.0.2a2cc74_r9_ad1',1)
-vslinput = find_idxSubHandle(vslname,'WindowsForms10.EDIT.app.0.2a2cc74_r9_ad1',0)
-win32gui.SendMessage(vslinput, win32con.WM_SETTEXT,None,'hu')
+docuwindow = win32gui.FindWindow('WindowsForms10.Window.8.app.0.3b93019_r11_ad1','Document ApplicationNBZRP0')
+backwindow = win32gui.FindWindowEx(docuwindow,0,'WindowsForms10.MDICLIENT.app.0.3b93019_r11_ad1',None)
+bookingwindow = win32gui.FindWindowEx(backwindow,0,'WindowsForms10.Window.8.app.0.3b93019_r11_ad1','Accept E-booking')
+bookingtitle = find_idxSubHandle(bookingwindow,'WindowsForms10.Window.8.app.0.3b93019_r11_ad1',4)
+retrieve = win32gui.FindWindowEx(bookingtitle,0,'WindowsForms10.BUTTON.app.0.3b93019_r11_ad1','Retrieve')
+vslvoy = find_idxSubHandle(bookingtitle,'WindowsForms10.Window.8.app.0.3b93019_r11_ad1',0)
+vslname = find_idxSubHandle(vslvoy,'WindowsForms10.Window.b.app.0.3b93019_r11_ad1',1)
+voyname = find_idxSubHandle(vslvoy,'WindowsForms10.Window.b.app.0.3b93019_r11_ad1',0)
+vslinput = find_idxSubHandle(vslname,'WindowsForms10.EDIT.app.0.3b93019_r11_ad1',0)
+voyinput = find_idxSubHandle(voyname,'WindowsForms10.EDIT.app.0.3b93019_r11_ad1',0)
+win32gui.SendMessage(vslinput, win32con.WM_SETTEXT,None,'PANHAON')
+time.sleep(.1)
+win32gui.SendMessage(voyinput, win32con.WM_SETTEXT,None,'222E')
+# win32gui.PostMessage(retrieve,win32con.BM_CLICK,None,None)
+win32gui.SendMessage(retrieve,win32con.BM_CLICK,None,None)
+#前台点击按钮
+# left, top, right, bottom = win32gui.GetWindowRect(retrieve)
+# win32api.SetCursorPos((left+15,top+15))
+# win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0, 0, 0)  
+# win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0,0,0, 0)
 
-# voyageinput = find_idxSubHandle(bookingwindow,'WindowsForms10.EDIT.app.0.2a2cc74_r9_ad1',0)
-# win32gui.SendMessage(vesselinput, win32con.WM_SETTEXT,None,'hu')
-# time.sleep(.2)
-# win32gui.SendMessage(voyageinput, win32con.WM_SETTEXT,None,'ko')
+
